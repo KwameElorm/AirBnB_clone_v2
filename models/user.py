98fae745898617e6b083278b7c5d63a8e8a56567
KwameElorm <elormh@ymail.com>
2024-06-06 00:26:25 +0000

    user.py

--- models/user.py
+++ models/user.py
@@ -1,35 +1,29 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-
+""" holds class User"""
+import models
 from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
+from os import getenv
+import sqlalchemy
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """
-    This class defines a user by various attributes
+    """Representation of a user """
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
 
-    Attributes:
-        __tablename__ (str): The table name for the database.
-        email (str): The email address of the user.
-        password (str): The password for the user's login.
-        first_name (str): The first name of the user.
-        last_name (str): The last name of the user.
-        places (relationship): One-to-many relationship with Place model.
-        reviews (relationship):One-to-many relationship with Review model.
-    """
-    __tablename__ = "users"
-
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-
-
-    places = relationship(
-        "Place", backref="user", cascade="all, delete, delete-orphan"
-    )
-    reviews = relationship(
-        "Review", backref="user", cascade="all, delete, delete-orphan"
-    )
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
