774a971e33e2d61aea94902c22be84a4ac214a5a
KwameElorm <elormh@ymail.com>
2025-01-06 02:40:14 -0800

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
-# B_script that sets web_static.
-sudo apt-get update
+# sets up the web servers for the deployment of web_static
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
 sudo apt-get -y install nginx
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo 'Hello Africa' > /data/web_static/releases/test/index.html
-ln -sfn /data/web_static/releases/test /data/web_static/current
-chown -R ubuntu:ubuntu /data/
-tag="server_name _;"
-c_tag="server_name _;\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
-sudo sed -i "s|$tag|$c_tag|" /etc/nginx/sites-available/default
-sudo service nginx restart
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx start
