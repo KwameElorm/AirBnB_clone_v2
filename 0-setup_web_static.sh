9baa4ae5fbf05613bde3cdbb14d32ac25535094f
KwameElorm <elormh@ymail.com>
2025-01-06 02:33:07 -0800

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,27 +1,13 @@
 #!/usr/bin/env bash
-# script that sets up web servers for the deployment of web_static
+# B_script that sets web_static.
 sudo apt-get update
 sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
-
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
-sudo echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
-
-sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
-
-sudo chown -R ubuntu:ubuntu /data/
-
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
-
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo 'Hello Africa' > /data/web_static/releases/test/index.html
+ln -sfn /data/web_static/releases/test /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+tag="server_name _;"
+c_tag="server_name _;\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
+sudo sed -i "s|$tag|$c_tag|" /etc/nginx/sites-available/default
 sudo service nginx restart
